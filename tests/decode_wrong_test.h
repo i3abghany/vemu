11498c423c63c95dfc317e12f618909daa7b254c
i3abghany <mahmoudabdalghany@outlook.com>
2021-02-03 13:00:52 +0200

    Added Wrong OPCode test

---
+++ tests/decode_wrong_test.h
@@ -0,0 +1,12 @@
+#include "../include/InstructionDecoder.h"
+
+int decode_wrong_test() {
+    uint32_t inst = 0x00000000;
+    auto a = InstructionDecoder::the().decode(inst);
+
+    if (a.get_type() != Instruction::Type::WRONG) {
+        return 1;
+    }
+
+    return 0;
+}
--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "decode_type_test.h"
+#include "decode_wrong_test.h"
 
 int main() {
     if (decode_type_test() != 0) {
         std::cout << "ERROR: decode_type_test() returned " << decode_type_test();
+        return 1;
+    }
+
+    if (decode_wrong_test() != 0) {
+        std::cout << "ERROR: decode_wrong_test() returned " << decode_wrong_test();
         return 1;
     }
 
