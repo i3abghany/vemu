a5998f58d665a2d96f8964d85b210b675e8c9455
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-02-18 22:05:10 +0200

    Added printing utility for instructions.

--- include/Instruction.h
+++ include/Instruction.h
@@ -1,5 +1,8 @@
 #pragma once
+
+#include <iostream>
 #include <cstdint>
+#include <string>
 #include "defs.h"
 
 struct Fields {
@@ -29,15 +32,54 @@ public:
 
 public:
     Instruction() = default;
-    Instruction(Type t, IName i, Fields vals)
-        : t(t), name(i), f(vals) {};
+    Instruction(Type t, IName i, Fields vals, std::string n)
+        : t(t), name(i), f(vals), str_name(n) {};
 
     Type get_type();
     Fields get_fields();
     IName get_name();
 
+	friend std::ostream& operator << (std::ostream &os, const Instruction &ins) {
+		os << std::hex;
+
+		if (ins.t == Type::WRONG) {
+			os << "Faulty Instruction.\n";
+			return os;
+		}
+
+		os << "Instruction: " << ins.str_name << " ";
+		os << "OPCode: " << static_cast<int>(ins.f.OPCode);
+
+		if (ins.t == Type::R) {
+			os << " rd: " << static_cast<int>(ins.f.rd);
+			os << " rs1: " << static_cast<int>(ins.f.rs1);
+			os << " rs2: " << static_cast<int>(ins.f.rs2);
+		} else if (ins.t == Type::I) {
+			os << " rd: " << static_cast<int>(ins.f.rd);
+			os << " rs1: " << static_cast<int>(ins.f.rs1);
+			os << " imm: 0x" << static_cast<int>(ins.f.imm);
+		} else if (ins.t == Type::S) {
+			os << " rs1: " << static_cast<int>(ins.f.rs1);
+			os << " rs2: " << static_cast<int>(ins.f.rs2);
+			os << " imm: 0x" << static_cast<int>(ins.f.imm);
+		} else if (ins.t == Type::B) {
+			os << " rs1: " << static_cast<int>(ins.f.rs1);
+			os << " rs2: " << static_cast<int>(ins.f.rs2);
+			os << " imm: 0x" << static_cast<int>(ins.f.imm);
+		} else if (ins.t == Type::U) {
+			os << " rd: " << static_cast<int>(ins.f.rd);
+			os << " imm: 0x" << static_cast<int>(ins.f.imm);
+		} else if (ins.t == Type::J) {
+			os << " rd: " << static_cast<int>(ins.f.rd);
+			os << " imm: 0x" << static_cast<int>(ins.f.imm);
+		} 
+		
+		return os << "\n";	
+	}
+
 private:
     Type t;
     IName name;
     Fields f;
+	std::string str_name;
 };
