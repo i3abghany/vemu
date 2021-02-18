97a2cddad25ed6b0580e929c2645b4c783691046
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-02-18 22:05:15 +0200

    Added a simple driver.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
-#include "../tests/decode_type_test.h"
+#include "../include/VEmu.h"
+#include <iostream>
+#include <string>
+#include "../include/InstructionDecoder.h"
 
 int main() {
+	std::string name = "./code.bin";
+	VEmu em { name };
+	uint32_t inst = em.run();
+
     return 0;
 }
