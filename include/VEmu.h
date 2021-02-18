4fdf27f2b5fefcec7bb3a81efd20bdc6254ce3ae
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-02-18 22:05:12 +0200

    Added basic emulator functionality.

--- include/VEmu.h
+++ include/VEmu.h
@@ -1,5 +1,27 @@
 #pragma once
 
-class VEmu {
+#include <vector>
+#include <array>
+#include <string>
+#include <cstdint>
+#include <fstream>
 
+#include "InstructionDecoder.h"
+
+class VEmu {
+public:
+	VEmu(std::string f_name);
+
+	uint32_t run();
+
+	void read_file();
+
+private:
+	constexpr static size_t REGS_NUM = 32;
+	std::array<uint64_t, REGS_NUM> regs;
+	
+	std::vector<uint8_t> code;
+
+	std::string bin_file_name;
 };
+
--- src/VEmu.cpp
+++ src/VEmu.cpp
@@ -1,1 +1,36 @@
 #include "../include/VEmu.h"
+#include <iostream>
+
+VEmu::VEmu(std::string f_name) :
+	bin_file_name(std::move(f_name)) 
+{
+	read_file();
+}
+
+void VEmu::read_file()
+{
+	std::ifstream ifs(bin_file_name, std::ios_base::binary);
+
+	char byte;
+
+	while ((byte = ifs.get()) && byte != EOF) {
+		code.push_back(static_cast<uint8_t>(byte));
+	}
+}
+
+uint32_t VEmu::run()
+{
+	for (int i = 0; i < code.size(); i += 4) {
+		uint32_t inst = code[i];
+		inst |= ((uint32_t)code[i + 1]) << 8;
+		inst |= ((uint32_t)code[i + 2]) << 16;
+		inst |= ((uint32_t)code[i + 3]) << 24;
+
+		auto decoded_inst = InstructionDecoder::the().decode(inst);
+
+		std::cout << decoded_inst;
+
+	}
+
+	return 0;
+}
