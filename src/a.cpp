152dfd43b83c396b9487759592f5e17b6d28b1a6
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-03-08 15:00:31 +0200

    Removed unnecessary files.

--- src/a.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdint>
-
-int main() {
-    int64_t a1 = 0x8000000000000000;
-    int64_t a2 = 0xffffffffffffffff;
-//    int64_t a3 = a1 / a2;
-    int64_t a4 = a2 / a1;
-}
