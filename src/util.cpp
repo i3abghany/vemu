f9f56bcf32129fe48dd7fd9d589d0f6d30af36ac
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-03-23 06:50:51 +0200

    util: replaced tabs with spaces.

--- src/util.cpp
+++ src/util.cpp
@@ -1,25 +1,25 @@
 #include "../include/util.h"
 
 uint64_t sext_byte(uint8_t b) {
-	return static_cast<uint64_t>(
-		static_cast<int64_t>(
-			static_cast<int8_t>(b)
-		)
-	);
+    return static_cast<uint64_t>(
+        static_cast<int64_t>(
+            static_cast<int8_t>(b)
+        )
+    );
 }
 
 uint64_t sext_hword(uint16_t hw) {
-	return static_cast<uint64_t>(
-		static_cast<int64_t>(
-			static_cast<int16_t>(hw)
-		)
-	);
+    return static_cast<uint64_t>(
+        static_cast<int64_t>(
+            static_cast<int16_t>(hw)
+        )
+    );
 }
 
 uint64_t sext_word(uint32_t w) {
-	return static_cast<uint64_t>(
-		static_cast<int64_t>(
-			static_cast<int32_t>(w)
-		)
-	);
+    return static_cast<uint64_t>(
+        static_cast<int64_t>(
+            static_cast<int32_t>(w)
+        )
+    );
 }
