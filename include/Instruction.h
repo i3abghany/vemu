69aec0702fd33cf2d2000f3b9c15a1ddd594d85e
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-03-23 06:52:14 +0200

    Instruction: Replaced tabs with spaces.

--- include/Instruction.h
+++ include/Instruction.h
@@ -40,49 +40,49 @@ public:
     Fields get_fields();
     IName get_name();
 
-	friend std::ostream& operator << (std::ostream &os, const Instruction &ins) {
-		os << std::hex;
-		os << std::setfill('0');
-		os << std::setw(8);
+    friend std::ostream& operator << (std::ostream &os, const Instruction &ins) {
+        os << std::hex;
+        os << std::setfill('0');
+        os << std::setw(8);
 
-		if (ins.t == Type::WRONG) {
-			os << "Faulty Instruction.\n";
-			return os;
-		}
+        if (ins.t == Type::WRONG) {
+            os << "Faulty Instruction.\n";
+            return os;
+        }
 
-		os << "Instruction: " << ins.str_name << " ";
-		os << "OPCode: " << static_cast<int>(ins.f.OPCode);
+        os << "Instruction: " << ins.str_name << " ";
+        os << "OPCode: " << static_cast<int>(ins.f.OPCode);
 
-		if (ins.t == Type::R) {
-			os << " rd: 0x" << static_cast<int>(ins.f.rd);
-			os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
-			os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
-		} else if (ins.t == Type::I) {
-			os << " rd: 0x" << static_cast<int>(ins.f.rd);
-			os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
-			os << " imm: 0x" << static_cast<int>(ins.f.imm);
-		} else if (ins.t == Type::S) {
-			os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
-			os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
-			os << " imm: 0x" << static_cast<int>(ins.f.imm);
-		} else if (ins.t == Type::B) {
-			os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
-			os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
-			os << " imm: 0x" << static_cast<int>(ins.f.imm);
-		} else if (ins.t == Type::U) {
-			os << " rd: 0x" << static_cast<int>(ins.f.rd);
-			os << " imm: 0x" << static_cast<int>(ins.f.imm);
-		} else if (ins.t == Type::J) {
-			os << " rd: 0x" << static_cast<int>(ins.f.rd);
-			os << " imm: 0x" << static_cast<int>(ins.f.imm);
-		} 
-		
-		return os << "\n";	
-	}
+        if (ins.t == Type::R) {
+            os << " rd: 0x" << static_cast<int>(ins.f.rd);
+            os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
+            os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
+        } else if (ins.t == Type::I) {
+            os << " rd: 0x" << static_cast<int>(ins.f.rd);
+            os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
+            os << " imm: 0x" << static_cast<int>(ins.f.imm);
+        } else if (ins.t == Type::S) {
+            os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
+            os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
+            os << " imm: 0x" << static_cast<int>(ins.f.imm);
+        } else if (ins.t == Type::B) {
+            os << " rs1: 0x" << static_cast<int>(ins.f.rs1);
+            os << " rs2: 0x" << static_cast<int>(ins.f.rs2);
+            os << " imm: 0x" << static_cast<int>(ins.f.imm);
+        } else if (ins.t == Type::U) {
+            os << " rd: 0x" << static_cast<int>(ins.f.rd);
+            os << " imm: 0x" << static_cast<int>(ins.f.imm);
+        } else if (ins.t == Type::J) {
+            os << " rd: 0x" << static_cast<int>(ins.f.rd);
+            os << " imm: 0x" << static_cast<int>(ins.f.imm);
+        }
+
+        return os << "\n";
+    }
 
 private:
     Type t;
     IName name;
     Fields f;
-	std::string str_name;
+    std::string str_name;
 };
