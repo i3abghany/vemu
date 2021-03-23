ecaa120ec508d31e0879ac973cfe57dd96975812
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-03-23 06:49:56 +0200

    Bus: Replaced tabs with spaces.

--- include/Bus.h
+++ include/Bus.h
@@ -4,9 +4,9 @@
 
 class Bus {
 public:
-	Bus();
-	uint64_t load(uint64_t, size_t);
-	void store(uint64_t, uint64_t, size_t);
+    Bus();
+    uint64_t load(uint64_t, size_t);
+    void store(uint64_t, uint64_t, size_t);
 private:
-	DRAM dr;
+    DRAM dr;
 };
