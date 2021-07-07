89a2d527afec46eff3937b4d10caa961ec962fbf
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-07-07 09:05:36 +0200

    CLIC: made store/load internal fns private.

--- include/CLIC.h
+++ include/CLIC.h
@@ -13,9 +13,10 @@ public:
     std::pair<uint64_t, ReturnException> load(uint64_t, size_t);
     ReturnException store(uint64_t, uint64_t, size_t);
 
+private:
     uint64_t load64(uint64_t addr);
     void store64(uint64_t addr, uint64_t data);
-private:
+
     uint64_t mtime;
     uint64_t mtimecmp;
 };
