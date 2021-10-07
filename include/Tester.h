e35d37988295156eacfe601504c9c210766622c3
Mahmoud Abd Al-Ghany <Mahmoudabdalghany@outlook.com>
2021-10-07 22:54:49 +0200

    Testing: Add a test skipping mechanism.
    A floating point addition test is added and skipped. (This fails for
    now because we do not set the flags appropriately yet)

--- include/Tester.h
+++ include/Tester.h
@@ -1,14 +1,19 @@
 #pragma once
 
-#include <cstdint>
-#include <map>
-#include <thread>
-
 #include <VEmu.h>
+
+
+struct TestCase {
+    std::string bin_file_name;
+    bool skip = false;
+
+    TestCase(std::string fname) : bin_file_name(std::move(fname)) {};
+    TestCase(std::string fname, bool to_skip) : bin_file_name(std::move(fname)), skip(to_skip) {}
+};
 
 class Tester {
 public:
     static void run();
 private:
-    const static std::vector<std::string> test_fnames;
+    const static std::vector<TestCase> test_cases;
 };
