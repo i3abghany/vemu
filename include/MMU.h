0b7aeef030ab7bc9bb9a1553687bd1d0b2d19c6b
Mahmoud Abumandour <ma.mandourr@gmail.com>
2023-04-20 00:02:09 -0700

    Bypass permissions checking for instruction loading

--- include/MMU.h
+++ include/MMU.h
@@ -29,18 +29,15 @@ public:
     MMU(uint64_t ram_size);
     [[nodiscard]] std::pair<uint64_t, ReturnException> load(uint64_t, size_t) override;
     ReturnException store(uint64_t, uint64_t, size_t) override;
-
     [[nodiscard]] uint64_t get_base() const override { return 0; }
-
     [[nodiscard]] uint64_t get_size() const override { return ram_size; }
-
     [[nodiscard]] bool is_interrupting() override { return false; }
-
     void write_from(const std::vector<uint8_t>&, uint64_t);
     [[nodiscard]] std::pair<std::vector<uint8_t>, ReturnException>
         read_to(uint64_t, uint64_t) const;
     [[nodiscard]] uint64_t cur_alloc_ptr() const { return alloc_ptr; }
     [[nodiscard]] std::string _read_null_terminated_string(uint64_t) const;
+    [[nodiscard]] std::pair<uint32_t, ReturnException> load_insn(uint64_t addr) const;
 
     void reset_to(const MMU& other);
 
